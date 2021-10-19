52391b4551e60ee359f0f8c38c6fcff317461c97
糖醋虾仁 <imcaviare@gmail.com>
2021-10-19 11:23:10 +0800

    feat scrypt默认加盐数据

--- utils/encrypt.go
+++ utils/encrypt.go
@@ -9,8 +9,12 @@ import (
 )
 
 // 传入文本信息加密文本
+// 默认salt：dingo
 func Encrypt(text string) string {
 	salt := os.Getenv("SALT")
+	if salt == "" {
+		salt = "dingo"
+	}
 	dk, err := scrypt.Key([]byte(text), []byte(salt), 32768, 8, 1, 32)
 	if err != nil {
 		log.Fatal(err)
